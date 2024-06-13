ffb3ecef741ec074de74a1497f9d0cb7f3ccf870
Ramazan <vipgunkz@gmail.com>
2024-06-13 16:15:53 +0500

    Initial Commit

---
+++ Kaspi.kz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kaspi.kz
+//
+//  Created by Ramazan Kalabay on 13.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
